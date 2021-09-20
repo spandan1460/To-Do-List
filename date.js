63beafd3857ad843c5485973106e77cb5a070c1c
spandan1460 <1929250@kiit.ac.in>
2021-09-20 22:57:30 +0530

    To-do List Upload

---
+++ date.js
@@ -0,0 +1,26 @@
+//jslint esversion:6
+
+exports.getDate = function() {
+    const today = new Date();
+
+    const options = {
+        weekday: "long",
+        day: "numeric",
+        month: "long",
+    };
+    let day = today.toLocaleDateString("en-US",options);
+    return day;
+}
+
+exports.getDay = function() {
+    const today = new Date();
+
+    const options = {
+        weekday: "long"
+    };
+    let day = today.toLocaleDateString("en-US",options);
+    return day;
+}
+
+
+console.log(module.exports);
